db2c93c098f69eef9335be5949e2e709cd9bc326
Efraim Gabuat <efgbt17@gmail.com>
2024-04-26 17:09:29 +0800

    feat(nctv-33): table layout stories added

--- projects/component-pantry/src/lib/components/table/table.component.ts
+++ projects/component-pantry/src/lib/components/table/table.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TableData } from './table';
+import { ColumnType } from './table';
 
 @Component({
     selector: 'nctv-table',
@@ -9,184 +11,46 @@ import { Component } from '@angular/core';
     styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
-    tableHeaders = [
-        { label: '#', sortable: false },
-        { label: 'Host Name', sortable: false },
-        { label: 'Dealer Name', sortable: false },
-        { label: 'Address', sortable: true },
-        { label: 'City', sortable: true },
-        { label: 'State', sortable: true },
-        { label: 'Postal Code', sortable: true },
-        { label: 'Timezone', sortable: true },
-        { label: 'Total License', sortable: true },
-    ];
+    /**
+     * Represents the data structure for the table, containing columns and rows.
+     */
+    @Input() tableData: TableData = {
+        columns: [],
+        rows: [],
+    };
 
-    tableData = [
-        [
-            {
-                value: 1,
-            },
-            {
-                value: 'Amazon Data Center',
-            },
-            {
-                value: 'Sunrise Motors Inc.',
-            },
-            {
-                value: '456 Elm Street Apt. 201',
-            },
-            {
-                value: 'Seattle',
-            },
-            {
-                value: 'Washington',
-            },
-            {
-                value: '98101',
-            },
-            {
-                value: 'Pacific',
-            },
-            {
-                value: 15,
-            },
-        ],
-        [
-            {
-                value: 2,
-            },
-            {
-                value: 'Microsoft Headquarters',
-            },
-            {
-                value: 'Sunset Electronics',
-            },
-            {
-                value: '789 Oak Avenue',
-            },
-            {
-                value: 'Redmond',
-            },
-            {
-                value: 'Washington',
-            },
-            {
-                value: '98052',
-            },
-            {
-                value: 'Pacific',
-            },
-            {
-                value: 20,
-            },
-        ],
-        [
-            {
-                value: 3,
-            },
-            {
-                value: 'Googleplex',
-            },
-            {
-                value: 'Skyline Solutions',
-            },
-            {
-                value: '123 Maple Street',
-            },
-            {
-                value: 'Mountain View',
-            },
-            {
-                value: 'California',
-            },
-            {
-                value: '94043',
-            },
-            {
-                value: 'Pacific',
-            },
-            {
-                value: 30,
-            },
-        ],
-        [
-            {
-                value: 4,
-            },
-            {
-                value: 'Apple Park',
-            },
-            {
-                value: 'Orchard Technologies',
-            },
-            {
-                value: '567 Pine Road',
-            },
-            {
-                value: 'Cupertino',
-            },
-            {
-                value: 'California',
-            },
-            {
-                value: '95014',
-            },
-            {
-                value: 'Pacific',
-            },
-            {
-                value: 25,
-            },
-        ],
-        [
-            {
-                value: 5,
-            },
-            {
-                value: 'Facebook Campus',
-            },
-            {
-                value: 'Social Solutions',
-            },
-            {
-                value: '101 Cedar Lane',
-            },
-            {
-                value: 'Menlo Park',
-            },
-            {
-                value: 'California',
-            },
-            {
-                value: '94025',
-            },
-            {
-                value: 'Pacific',
-            },
-            {
-                value: 18,
-            },
-        ],
-    ];
+    /**
+     * Specifies the type of columns to be displayed in the table. Possible values: 'full', 'compact'.
+     * Default value is 'full'.
+     */
+    @Input() columnType: ColumnType = 'full';
 
+    /**
+     * Represents the index of the currently sorted column.
+     * Default value is -1, indicating no column is currently sorted.
+     */
     columnIndex: number = -1;
+
+    /**
+     * Represents the sorting order for the currently sorted column.
+     * Default value is true, indicating ascending order.
+     */
     ascending: boolean = true;
 
+    /**
+     * Sorts the table rows based on the provided column index.
+     * @param columnIndex The index of the column to be sorted.
+     */
     sortColumn(columnIndex: number) {
-        // Check if the selected column is sortable
-        if (this.tableHeaders[columnIndex].sortable) {
-            // Set the selected column index
+        if (this.tableData.columns[columnIndex].sortable) {
             this.columnIndex = columnIndex;
 
-            // Toggle the sorting order
             this.ascending = !this.ascending;
 
-            // Sort the table data based on the selected column
-            this.tableData.sort((a, b) => {
-                const valueA: number | string = a[columnIndex].value;
-                const valueB: number | string = b[columnIndex].value;
+            this.tableData.rows.sort((rowA, rowB) => {
+                const valueA: any = rowA[columnIndex].value;
+                const valueB: any = rowB[columnIndex].value;
 
-                // Check if the values are strings or numbers and compare accordingly
                 if (typeof valueA === 'string' && typeof valueB === 'string') {
                     return this.ascending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
                 } else {
@@ -194,5 +58,18 @@ export class TableComponent {
                 }
             });
         }
+    }
+
+    /**
+     * Generates class names based on the column type.
+     * Constructs an object suitable for ngClass based on `columnType`.
+     * @returns {Object} Object with dynamic class names
+     */
+    getClass() {
+        const classes = {
+            [`column--type-${this.columnType}`]: this.columnType,
+        };
+
+        return classes;
     }
 }
--- projects/component-pantry/src/lib/components/table/table.ts
+++ projects/component-pantry/src/lib/components/table/table.ts
@@ -1,1 +1,19 @@
-export const Table = [];
+// Define an interface for a single column
+export interface TableColumn {
+    label: string;
+    sortable: boolean;
+}
+
+// Define an interface for a single row
+export interface TableRow {
+    value: any;
+}
+
+// Define an interface for the table data
+export interface TableData {
+    columns: TableColumn[];
+    rows: TableRow[][];
+}
+
+export type ColumnType = 'full' | 'ellipsis' | 'wrap';
+export const COLUMN_TYPE: ColumnType[] = ['full', 'ellipsis', 'wrap'];
