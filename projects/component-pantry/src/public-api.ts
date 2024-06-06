1ac395ab7bb3c26806cc86d15388302cedcdec89
earlvhin <earlgabuat@gmail.com>
2024-06-06 12:11:20 +0800

    chore: switch INPUT_SIZE to AUTOCOMPLETE_INPUT_SIZE for Autocomplete component sizes, update public-api

--- projects/component-pantry/src/public-api.ts
+++ projects/component-pantry/src/public-api.ts
@@ -2,4 +2,12 @@
  * Public API Surface of component-pantry
  */
 
+export * from './lib/components/autocomplete';
 export * from './lib/components/button';
+export * from './lib/components/calendar';
+export * from './lib/components/card';
+export * from './lib/components/card';
+export * from './lib/components/input';
+export * from './lib/components/stepper';
+export * from './lib/components/table';
+export * from './lib/components/text';
--- projects/component-pantry/src/stories/autocomplete.stories.ts
+++ projects/component-pantry/src/stories/autocomplete.stories.ts
@@ -1,7 +1,7 @@
 import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../lib/components/card';
-import { AutocompleteComponent, INPUT_SIZE } from '../lib/components/autocomplete';
+import { AUTOCOMPLETE_INPUT_SIZE, AutocompleteComponent } from '../lib/components/autocomplete';
 import { InputComponent } from '../lib/components/input';
 import { ButtonComponent } from '../public-api';
 
@@ -19,7 +19,7 @@ const meta: Meta<AutocompleteComponent> = {
         inputSize: {
             control: 'select',
             description: 'Used to set input size.',
-            options: INPUT_SIZE,
+            options: AUTOCOMPLETE_INPUT_SIZE,
             table: {
                 defaultValue: {
                     summary: 'medium',
@@ -71,9 +71,9 @@ export const Basic: Story = {
             <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>`,
         rightIconSvg: `<svg width="15" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path 
+        <path
           clip-rule="evenodd"
-          d="M9.53033 9.28033C9.23744 9.57322 8.76256 9.57322 8.46967 9.28033L0.96967 1.78033C0.676777 1.48744 0.676777 1.01256 0.96967 0.719671C1.26256 0.426777 1.73744 0.426777 2.03033 0.719671L9 7.68934L15.9697 0.71967C16.2626 0.426777 16.7374 0.426777 17.0303 0.71967C17.3232 1.01256 17.3232 1.48744 17.0303 1.78033L9.53033 9.28033Z" 
+          d="M9.53033 9.28033C9.23744 9.57322 8.76256 9.57322 8.46967 9.28033L0.96967 1.78033C0.676777 1.48744 0.676777 1.01256 0.96967 0.719671C1.26256 0.426777 1.73744 0.426777 2.03033 0.719671L9 7.68934L15.9697 0.71967C16.2626 0.426777 16.7374 0.426777 17.0303 0.71967C17.3232 1.01256 17.3232 1.48744 17.0303 1.78033L9.53033 9.28033Z"
         >
         </path>
       </svg>`,
@@ -135,23 +135,23 @@ export const MultipleAutocomplete: Story = {
             <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); gap: 20px;">
                 <nctv-autocomplete [inputSize]="inputSize" [label]="label" [placeholder]="placeholder" [autocompleteData]="autocompleteData">
                 </nctv-autocomplete>
-                
-                <nctv-input [label]="label" [inputSize]="inputSize" [placeholder]="placeholder"></nctv-input>
-                
-                <nctv-input [label]="'I am a label (Static)'" [inputSize]="inputSize" [placeholder]="'I am a placeholder (Static)'"></nctv-input>
-                
+
                 <nctv-input [label]="label" [inputSize]="inputSize" [placeholder]="placeholder"></nctv-input>
 
-                <nctv-autocomplete [inputSize]="inputSize" [label]="'Second autocomplete (Static)'" [placeholder]="'Select animal (Static)'" [autocompleteData]="[{id: '1', name: 'Lion', species: 'Panthera leo'}, {id: '2', name: 'Tiger', species: 'Panthera tigris'}, {id: '3', name: 'Elephant', species: 'Loxodonta'}, {id: '4', name: 'Giraffe', species: 'Giraffa camelopardalis'}, {id: '5', name: 'Zebra', species: 'Equus zebra'}]">
-                </nctv-autocomplete>              
-                
                 <nctv-input [label]="'I am a label (Static)'" [inputSize]="inputSize" [placeholder]="'I am a placeholder (Static)'"></nctv-input>
-                
+
+                <nctv-input [label]="label" [inputSize]="inputSize" [placeholder]="placeholder"></nctv-input>
+
                 <nctv-autocomplete [inputSize]="inputSize" [label]="'Second autocomplete (Static)'" [placeholder]="'Select animal (Static)'" [autocompleteData]="[{id: '1', name: 'Lion', species: 'Panthera leo'}, {id: '2', name: 'Tiger', species: 'Panthera tigris'}, {id: '3', name: 'Elephant', species: 'Loxodonta'}, {id: '4', name: 'Giraffe', species: 'Giraffa camelopardalis'}, {id: '5', name: 'Zebra', species: 'Equus zebra'}]">
                 </nctv-autocomplete>
 
                 <nctv-input [label]="'I am a label (Static)'" [inputSize]="inputSize" [placeholder]="'I am a placeholder (Static)'"></nctv-input>
-                
+
+                <nctv-autocomplete [inputSize]="inputSize" [label]="'Second autocomplete (Static)'" [placeholder]="'Select animal (Static)'" [autocompleteData]="[{id: '1', name: 'Lion', species: 'Panthera leo'}, {id: '2', name: 'Tiger', species: 'Panthera tigris'}, {id: '3', name: 'Elephant', species: 'Loxodonta'}, {id: '4', name: 'Giraffe', species: 'Giraffa camelopardalis'}, {id: '5', name: 'Zebra', species: 'Equus zebra'}]">
+                </nctv-autocomplete>
+
+                <nctv-input [label]="'I am a label (Static)'" [inputSize]="inputSize" [placeholder]="'I am a placeholder (Static)'"></nctv-input>
+
                 <nctv-autocomplete [inputSize]="inputSize" [label]="'Second autocomplete (Static)'" [placeholder]="'Select animal (Static)'" [autocompleteData]="[{id: '1', name: 'Lion', species: 'Panthera leo'}, {id: '2', name: 'Tiger', species: 'Panthera tigris'}, {id: '3', name: 'Elephant', species: 'Loxodonta'}, {id: '4', name: 'Giraffe', species: 'Giraffa camelopardalis'}, {id: '5', name: 'Zebra', species: 'Equus zebra'}]">
                 </nctv-autocomplete>
             </div>
